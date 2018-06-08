98c94eea935cb8772be83ad6a91af76ee834dbef
bxh <2216458305@qq.com>
2018-06-08 17:14:29 +0800

    第二次提交

---
+++ src/main/java/com/baizhi/entity/User.java
@@ -0,0 +1,25 @@
+package com.baizhi.entity;
+
+/**
+ * Created by Administrator on 2018/6/8.
+ */
+public class User {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
